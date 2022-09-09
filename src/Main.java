c362ec60786a4c20ffe5cc1cf16c58e23b3ef31c
yasirsaglam <yasirsaglamm@gmail.com>
2022-09-09 20:43:43 +0300

    "R1"

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,24 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.print("Sayı giriniz : ");
         n = input.nextInt();
-
-        if (n < 2) {
-            System.out.println(n + " asal sayı değildir");
+        x = prime(n, 2);
+        if (x == 1) {
+            System.out.println(n + " asal sayıdır");
         } else {
-            x = prime(n, 2);
-            if (x == 1) {
-                System.out.println(n + " asal sayıdır");
-            } else {
-                System.out.println(n + " asal sayı değildir");
-            }
+            System.out.println(n + " asal sayı değildir");
         }
     }
+
     static int prime(int y, int i) {
-        if (i < y) {
-            if (y % i != 0) {
-                return (prime(y, ++i));
-            } else {
-                return 0;
+        if (y < 2) {
+            return 0;
+        } else {
+            if (i < y) {
+                if (y % i != 0) {
+                    return (prime(y, ++i));
+                } else {
+                    return 0;
+                }
             }
         }
         return 1;
